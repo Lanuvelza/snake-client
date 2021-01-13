8d71b31d0e6a42e11353e6224fa5f122ccc9f637
Lanuvelza <ianlam1221@hotmail.com>
2021-01-13 21:01:52 +0000

    updated setupInput function to accept a conn parameter

--- input.js
+++ input.js
@@ -1,10 +1,14 @@
 const stdin = process.stdin;
 
+// Stores the active TCP connection object.
+let connection;
+
 /**
  * Setup User Interface 
  * Specifically, so that we can handle user input via stdin
  */
-const setupInput = function() {
+const setupInput = function(conn) {
+  connection = conn;
   stdin.setRawMode(true);
   stdin.setEncoding('utf8');
   stdin.resume();
@@ -16,8 +20,18 @@ const handleUserInput = function() {
   stdin.on('data', (key) => {
     if (key === '\u0003') {
       process.exit();
+    } else if (key === 'w') {
+      connection.write("Move: up"); 
+    } else if (key === "a") {
+      connection.write("Move: left")
+    } else if (key === "d") {
+      connection.write("Move: right");
+    } else if (key === "s") {
+      connection.write("Move: down");
     }
   });
 }
+
+
 
 module.exports = { setupInput };
